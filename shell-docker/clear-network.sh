c83c2f66d18e365f561e51c3dc1b02f19ce7ceac
guoshuai <767410530@qq.com>
2018-01-16 19:35:10 +0800

    xiugai clear-network

--- shell-docker/clear-network.sh
+++ shell-docker/clear-network.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 container=$(docker ps | awk '{print $1}' | sed '1d')
-docker stop $container
-net=$(docker network ls | awk '{print $1}' | sed '1d')
-docker network rm $net
+if [ -n "$container" ];then
+     docker stop $container
+     net=$(docker network ls | awk '{print $2}' | sed '1d' | grep -v 'bridge' | grep -v 'host' | grep -v 'none')
+     if [ -n "$net" ];then
+         docker network rm $net
+     fi
+         
+else 
+     net=$(docker network ls | awk '{print $2}' | sed '1d' | grep -v 'bridge' | grep -v 'host' | grep -v 'none')
+     docker network rm $net
+fi
 
 
