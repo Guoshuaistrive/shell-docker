bfe1c95fbe56a3a6586a06d4371f492d797ca4cd
guoshuai <767410530@qq.com>
2018-01-16 20:14:46 +0800

    zuizhong

--- shell-docker/clear-all.sh
+++ shell-docker/clear-all.sh
@@ -8,14 +8,14 @@ if [ -n "$container" ];then
      fi
      gg=$(docker ps -a | awk '{print $1}' | sed '1d')
      docker rm $gg
-     images=$(docker images | awk '{print $3}')
+     images=$(docker images | awk '{print $3}' | sed '1d')
      docker rmi $images     
 else 
      net=$(docker network ls | awk '{print $2}' | sed '1d' | grep -v 'bridge' | grep -v 'host' | grep -v 'none')
      if [ -n "$net" ];then
          docker network rm $net
      fi
-     images=$(docker images | awk '{print $3}')
+     images=$(docker images | awk '{print $3}' | sed '1d')
      docker rmi $images
      gg=$(docker ps -a | awk '{print $1}' | sed '1d')
      docker rm $gg
